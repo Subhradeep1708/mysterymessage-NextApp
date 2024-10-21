648874e556e5abb0443ceba3435483eda7c8ba65
Subhradeep1708 <subhradeep1708@gmail.com>
2024-10-22 01:17:58 +0530

    Done upto half portion of route definition

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,25 +2,66 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 
-
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
 export async function POST(request: Request) {
+  await dbConnect();
 
-    await dbConnect()
+  try {
+    const { username, email, password } = await request.json();
+    // Checking if user is present in db or not
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
 
-    try {
-        const { username, email, password } = await request.json()
-    } catch (error) {
-        console.log("Error registering user", error);
-        return Response.json({
-            success: false,
-            message: 'Error registering user'
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
         },
-            {
-                status: 500
-            }
-        )
-
+        { status: 400 }
+      );
     }
+
+    const existingUserByEmail = await UserModel.findOne({ email });
+    // generating otp
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserByEmail) {
+      true;
+    } else {
+      // password hashing and setting a expiry time for verifyCode
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      // creating User in the db
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+      await newUser.save();
+    }
+  } catch (error) {
+    console.log("Error registering user", error); // this will be on terminal
+    return Response.json(
+      {
+        // returned will be sent to frontend
+        success: false,
+        message: "Error registering user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
